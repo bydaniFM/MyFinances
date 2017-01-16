57b92095340a1026dbf36338587515ae75d8ebe3
DanielFM <daniFM333@gmail.com>
2017-01-16 23:29:13 +0100

    Start xml implementation

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+
+    private DataLoader data;
+
+    private int balance;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Balance:"+balance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour {
+
+    public string path;
+    string settingsPath;
+    private Data data;
+
+    public static DataLoader instance;
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            Debug.LogError("An instance of RulesLoader already exists");
+        }
+        //settingsPath = Application.dataPath + "/Settings/";
+        //#if UNITY_STANDALONE_WIN
+			     //   path = path.Replace('/', '\\');
+        //#endif
+
+        //#if !UNITY_EDITOR
+		      //  FirstRun();
+
+		      //  rules = Rules.Load(File.ReadAllText(settingsPath+"_rules.xml"));
+        //#else
+        //        rules = Rules.Load(Resources.Load<TextAsset>("Rules").text);
+        //#endif
+    }
+    void Start() {
+        Debug.Log(data.balance);
+    }
+}
